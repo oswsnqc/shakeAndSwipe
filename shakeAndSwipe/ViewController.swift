75e5d843f10f4a319dbba8283f7769aae4c78261
Yisen Jing <jyisen@hotmail.com>
2016-05-16 22:49:05 -0700

    learn swipe and shake in IOS

--- shakeAndSwipe/ViewController.swift
+++ shakeAndSwipe/ViewController.swift
@@ -12,8 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        var swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDown)
+        
+        
     }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        
+        if event.subtype == UIEventSubtype.MotionShake {
+            println("user shaked!!!")
+            
+        }
+    }
+    
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                println("user swiped right")
+            case UISwipeGestureRecognizerDirection.Left:
+                println("user swiped left")
+            case UISwipeGestureRecognizerDirection.Up:
+                println("user swiped up")
+            case UISwipeGestureRecognizerDirection.Down:
+                println("user swiped down")
+            default:
+                break
+                
+                
+            }
+        }
+        
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
